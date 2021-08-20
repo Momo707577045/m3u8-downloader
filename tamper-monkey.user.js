e59c74ccbd4450af7452650aadb5aaeaa2b15f03
Sean Zhang <zxf2342@qq.com>
2021-08-20 11:39:10 +0800

    Update tamper-monkey.user.js

--- tamper-monkey.user.js
+++ tamper-monkey.user.js
@@ -4,7 +4,7 @@
 // @version      0.5.0
 // @description  https://github.com/Momo707577045/m3u8-downloader 配套插件
 // @author       Momo707577045
-// @match        *
+// @include      *
 // @exclude      http://blog.luckly-mjw.cn/tool-show/m3u8-downloader/index.html
 // @grant        none
 // @run-at       document-idle
