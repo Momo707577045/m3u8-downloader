3b8b65341d432dd244250235469eb11980d33438
Sean Zhang <zxf2342@qq.com>
2021-08-20 11:22:26 +0800

    Change @include to @match, @run-at document-idle

--- tamper-monkey.js
+++ tamper-monkey.js
@@ -4,10 +4,10 @@
 // @version      0.5.0
 // @description  https://github.com/Momo707577045/m3u8-downloader 配套插件
 // @author       Momo707577045
-// @include      *
+// @match        *
 // @exclude      http://blog.luckly-mjw.cn/tool-show/m3u8-downloader/index.html
 // @grant        none
-// @run-at document-start
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
